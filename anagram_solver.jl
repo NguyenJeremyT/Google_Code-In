c608c43378a3696cc771e16621da85d1e0740ae3
NguyenJeremyT <43259194+NguyenJeremyT@users.noreply.github.com>
2019-12-11 19:19:04 -0800

    added comments + cleaned up code

--- anagram_solver.jl
+++ anagram_solver.jl
@@ -1,4 +1,5 @@
 function is_anagram(x, y)
+	# converts both strings into an array of letters and then sorts the array
 	if sort(split(x, "")) == sort(split(y, ""))
 		return true end
 	return false
@@ -7,23 +8,25 @@ end
 function download_words()
 	download("https://github.com/tk3369/words/raw/master/words.txt", "words.txt")
 	f = open("words.txt");
+
+	# creates an array for each word on a new line
 	lines = readlines(f)
-	counter = 1
-	for l in lines
-		lowercase("$counter $l")
-		counter += 1
+
+	# iterates through the array and lowercases each word
+	for i in lines
+		lowercase(i)
 	end
 	return lines
 end
 
 function find_anagrams(x)
-	a = String[]
 	lines = download_words()
+
+	# iterates through the entire word list - if any words are anagrams it is pushed to an array
+	a = String[]
 	for i in lines
 		if is_anagram(x, i)
 			push!(a, i) end
 	end
 	return a
 end
-
-find_anagrams("iceman")
